e50018ca4f3810812c27545d3da4ffaa6ec4fddc
ebrahim hanna <ebrahim hanna>
2017-03-02 16:49:02 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtstring = new System.Windows.Forms.TextBox();
+            this.txtsubstring = new System.Windows.Forms.TextBox();
+            this.txtreplace = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtstring
+            // 
+            this.txtstring.Location = new System.Drawing.Point(101, 12);
+            this.txtstring.Multiline = true;
+            this.txtstring.Name = "txtstring";
+            this.txtstring.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtstring.Size = new System.Drawing.Size(259, 79);
+            this.txtstring.TabIndex = 0;
+            // 
+            // txtsubstring
+            // 
+            this.txtsubstring.Location = new System.Drawing.Point(101, 97);
+            this.txtsubstring.Name = "txtsubstring";
+            this.txtsubstring.Size = new System.Drawing.Size(259, 20);
+            this.txtsubstring.TabIndex = 1;
+            // 
+            // txtreplace
+            // 
+            this.txtreplace.Location = new System.Drawing.Point(101, 123);
+            this.txtreplace.Name = "txtreplace";
+            this.txtreplace.Size = new System.Drawing.Size(259, 20);
+            this.txtreplace.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "تبديل";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(101, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "حذف";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "النص";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "كلمة البحث";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "كلمة الاستبدال";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(372, 214);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtreplace);
+            this.Controls.Add(this.txtsubstring);
+            this.Controls.Add(this.txtstring);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(388, 253);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(388, 253);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.TextBox txtstring;
+        private System.Windows.Forms.TextBox txtsubstring;
+        private System.Windows.Forms.TextBox txtreplace;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strsubstring = txtsubstring.Text;
+            string strreplacestring = txtreplace.Text;
+            string strstring = txtstring.Text;
+            int nsslenth = strsubstring.Length;
+            int nsrlength = strreplacestring.Length;
+            int nsindex = strstring.IndexOf(strsubstring);
+            if (nsslenth == 0)
+            {
+                MessageBox.Show("يرجا ادخال نص", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (nsindex == -1)
+            {
+                MessageBox.Show("يرجا ادخال كلمة او محرف موجودة في النص", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (nsrlength == 0)
+            {
+                MessageBox.Show("يرجا ادخال كلمة او محرف المراد التبديل بها", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtstring.Text = strstring.Replace(strsubstring, strreplacestring);
+                txtsubstring.Clear();
+                txtreplace.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strsubstring = txtsubstring.Text;
+            string strstring = txtstring.Text;
+            int nsslenth = strsubstring.Length;
+            int nsindex = strstring.IndexOf(strsubstring);
+
+            if(nsslenth==0)
+            {
+                MessageBox.Show("يرجا ادخال نص", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (nsindex==-1)
+            {
+                MessageBox.Show("يرجا ادخال كلمة او محرف موجودة في النص", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtstring.Text = strstring.Remove(nsindex, nsslenth);
+                txtsubstring.Clear();
+            }
+        }
+    }
+}
